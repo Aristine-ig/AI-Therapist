d36f5eddf000127d240d52cb39fc56d6e7052014
Aristine-ig <aristine277@gmail.com>
2025-08-12 17:30:51 +0530

    commit

--- components/auth/sign-in-button.tsx
+++ components/auth/sign-in-button.tsx
@@ -1,15 +1,16 @@
 "use client";
+
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 interface SignInButtonProps {
-    className?: string;
+  className?: string;
 }
 
-export default function SignInButton({ className} : SignInButtonProps) {
-    return (
-        <Button asChild className={className}>
-            <Link href="/login">Sign In</Link>
-        </Button>
-    )
+export function SignInButton({ className }: SignInButtonProps) {
+  return (
+    <Button asChild className={className}>
+      <Link href="/login">Sign In</Link>
+    </Button>
+  );
 }
